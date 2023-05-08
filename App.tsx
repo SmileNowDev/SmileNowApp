039eef6781d711e0cec90fc42c08d5fb5b8dfc91
Brendan Probst <73138670+brendanprobst@users.noreply.github.com>
2023-05-08 13:03:47 -0400

    home page

--- App.tsx
+++ App.tsx
@@ -1,30 +1,24 @@
-import React, { useContext } from "react";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider as MyProvider } from "./providers/provider";
 import AppLoading from "expo-app-loading";
 import { StatusBar } from "expo-status-bar";
 
-import { BeansProvider, extendTheme } from "rn-beans-ui";
-import { myCustomTheme } from "./styles/beansTheme";
 import { loadFonts } from "./utils/loadFonts";
 import RootNavigator from "./navigation/rootNavigator";
-import { useColorScheme, Text } from "react-native";
 
 export default function App() {
-	const colorScheme = useColorScheme();
 	let fontsLoaded = loadFonts();
 	if (!fontsLoaded) {
 		return <AppLoading />;
 	} else {
 		return (
-			<BeansProvider customTheme={myCustomTheme}>
-				<MyProvider>
-					<StatusBar style="auto" />
-					<NavigationContainer>
-						<RootNavigator />
-					</NavigationContainer>
-				</MyProvider>
-			</BeansProvider>
+			<MyProvider>
+				<StatusBar style="auto" />
+				<NavigationContainer>
+					<RootNavigator />
+				</NavigationContainer>
+			</MyProvider>
 		);
 	}
 }
--- pages/home.tsx
+++ pages/home.tsx
@@ -5,13 +5,9 @@ import {
 	TouchableOpacity,
 	View,
 	Platform,
-	Alert,
-	ScrollView,
-	RefreshControl,
 	FlatList,
-	useColorScheme,
 } from "react-native";
-import { Fonts } from "../styles/theme";
+import { Fonts, Colors } from "../styles/theme";
 import { ButtonStyles, GlobalStyles } from "../styles/styles";
 import PartyListItem from "../components/partyListItem";
 import HomeHeader from "../components/homeHeader";
@@ -23,12 +19,8 @@ import * as Device from "expo-device";
 import { getInitials } from "./friends";
 import ScreenWrapper from "../components/core/screenWrapper";
 import WelcomeMessage from "../components/info/welcomeMessage";
-import { useColors, useFonts } from "rn-beans-ui";
 export default function HomePage({ navigation }) {
-	const colorScheme = useColorScheme();
-	const Colors = useColors();
-	const Fonts = useFonts();
-	// console.log(Colors);
+	//todo: give events a type
 	const [events, setEvents] = useState([]);
 	const [refreshing, setRefreshing] = useState(false);
 	const [page, setPage] = useState(1); // Add this state
